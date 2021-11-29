d2869448ba4a048c4a09cb5c91aa19392a8f7023
Maria M <prov21_1@hotmail.com>
2021-11-28 19:13:54 -0700

    some pseudocode

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,19 @@
 
 -- main :: IO ()
 -- main = someFunc
+
+
+-- HELPER Functions
+
+-- check if position is open 
+
+-- place piece in position chosen
+
+-- check if player won
+
+-- print board with newly placed piece
+
+
+-- alternate turns
+
+-- prevent replays
